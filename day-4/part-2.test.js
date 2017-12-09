52d01258fa1442c1367a95a186a2fb38ab87b4a4
Preston Richey <prichey@barkleyus.com>
2017-12-09 01:20:29 -0600

    Add tests for day-4 part 2

--- day-4/part-2.test.js
+++ day-4/part-2.test.js
@@ -2,38 +2,27 @@ const countValidPasswords = require('./part-2');
 const utils = require('./utils');
 
 test('example 1', () => {
-  expect(countValidPasswords('aa bb cc dd aa')).toBe(0);
+  expect(
+    countValidPasswords(`
+      aab aba
+      bb bb`)
+  ).toBe(0);
 });
 
 test('example 2', () => {
-  expect(countValidPasswords('aa bb cc dd aaa')).toBe(1);
+  expect(
+    countValidPasswords(`
+    acd dca
+    ac bc
+  `)
+  ).toBe(1);
 });
 
 test('example 3', () => {
   expect(
-    countValidPasswords(`aa bb
-    cc dd`)
-  ).toBe(2);
-});
-
-test('example 4', () => {
-  expect(
-    countValidPasswords(`aa bb
-    cc cc`)
-  ).toBe(1);
-});
-
-test('example 5', () => {
-  expect(
-    countValidPasswords(`aa aa
-    cc cc`)
-  ).toBe(0);
-});
-
-test('example 6', () => {
-  expect(
-    countValidPasswords(`aa bb
-    cc cc
-    dd ee`)
+    countValidPasswords(`
+    bcd def
+    gha ahe
+  `)
   ).toBe(2);
 });
